ad95a6666087ee63ebfa9bc5c7d86e1e47842af8
startoverrox <toplondon08@gmail.com>
2025-06-16 13:37:14 +0900

    chore: revert changes of vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  base: '/portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
