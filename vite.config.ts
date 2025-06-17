178c20c551050f383ff489d7573cbdffb6cece37
startoverrox <toplondon08@gmail.com>
2025-06-17 13:13:56 +0900

    chore: vite plugin alias added

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import alias from '@rollup/plugin-alias';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    alias({
+      entries: [{ find: '@', replacement: '/src' }],
+    })
+  ],
 });
